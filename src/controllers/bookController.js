766bc4ffdf91832fd4c7cea0afd8faf3b0f346e4
Nishant8826 <106534755+Nishant8826@users.noreply.github.com>
2022-06-07 11:50:49 +0530

    Update bookController.js

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,31 +1,38 @@
 
 const BookModel = require("../models/bookModel.js")
-
+//Q.1
 const getBookData = async function (req, res) {
     var data = req.body
     let savedData = await BookModel.create(data)
     res.send({ msg: savedData })
 }
-
+//Q.2
 const newBookData = async function (req, res) {
-    let allBook = await BookModel.find().select({ bookName: 1, authorName: 1, _id: 0 })
+    let allBook = await BookModel.find().select({ BookName: 1, AuthorName: 1, _id: 0 })
     res.send({ msg: allBook })
+}
+//Q.3
+const getBookInYear = async function (req, res) {
+    let year=req.body.Year;
+    let allBook = await BookModel.find({ Year: year });
+    res.send({ msg: allBook });
 }
 
-const getBookInYear = async function (req, res) {
-    let allBook = await BookModel.find({ year: req.body.year })
-    res.send({ msg: allBook })
-}
+//Q.4
 const getParticularBooks = async function (req, res) {
     let allBook = await BookModel.find(req.body)
     res.send({ msg: allBook })
 }
+
+//Q.5
 const getXINRBook = async function (req, res) {
-    let allBook = await BookModel.find({ 'price:indianPrice': { $in: ["100", "200", "500"] } })
+    let allBook = await BookModel.find({$or:[{"prices":"₹100"},{"prices":"₹200"},{"prices":"₹500"}]})
     res.send({ msg: allBook })
 }
+
+//Q.6
 const getRandomBooks = async function (req, res) {
-    const allBook = await BookModel.find({ $or: [{ stockAvailable: true }, { totalPages: { $gt: 500 } }] })
+    const allBook = await BookModel.find({ $or: [{ StockAvailble: true }, { TotalPages: { $gt: 500 } }] })
     res.send({ msg: allBook })
 }
 
