840e361b053b10136e8a47fdf54bb59b0c43ec5b
Nishant8826 <106534755+Nishant8826@users.noreply.github.com>
2022-06-07 12:21:17 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,16 +4,16 @@ const BookController= require("../controllers/bookController")
 
 
 
-router.post('/createBook',  BookController.getBookData  );
+router.post('/createBook',  BookController.getBookData  );                  //Q.1
 
-router.get('/getBooks',  BookController.newBookData  );
+router.get('/getBooks',  BookController.newBookData  );                     //Q.2
 
-router.post('/getBookInYear',  BookController.getBookInYear  );
+router.post('/getBookInYear',  BookController.getBookInYear  );             //Q.3
 
-router.post('/getParticularBooks',  BookController.getParticularBooks  );
+router.post('/getParticularBooks',  BookController.getParticularBooks  );   //Q.4
 
-router.get('/getXINRBook',  BookController.getXINRBook  );
+router.get('/getXINRBook',  BookController.getXINRBook  );                  //Q.5
 
-router.get('/getRandomBook',  BookController.getRandomBooks  );
+router.get('/getRandomBook',  BookController.getRandomBooks  );             //Q.6
  
 module.exports = router;
