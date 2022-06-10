bec640764f74df6e7f1a4e4ec10cf6dd0bccdef3
Nishant8826 <rnishant609@gmail.com>
2022-06-11 02:06:10 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -8,18 +8,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Nishant-R:cMVSc6ePV6V4dr03@cluster0.rembes2.mongodb.net/Nishant-Rathore", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+    function(req,res,next){
+    let today=new Date();
+    // console.log(today)
+    let date=today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate()+" ";
+    let time=today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    const dateAndTime=date + time;
+    const ip=req.ip;
+    const path=req.path;
+    console.log(`Current Date&Time:${dateAndTime}\nIP Address==>${ip}\nPath==>${path}`);
+    next();
+}
+);
 
 app.use('/', route);
 
