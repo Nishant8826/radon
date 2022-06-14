0a0c9c039e41642c02fa411ef7a99765887841d0
Nishant8826 <rnishant609@gmail.com>
2022-06-15 00:09:05 +0530

    Authentication

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Nishant-R:cMVSc6ePV6V4dr03@cluster0.rembes2.mongodb.net/14-JUNE", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
